2067662de4a79665668cdebad73113defd05fa28
Yalanskyi Roman <keito.eternal@gmail.com>
2024-09-08 22:29:58 +0300

    basic config for model, future training

---
+++ Transformers/VisionTransformer/config.py
@@ -0,0 +1,20 @@
+import torch
+
+BATCH_SIZE = 512
+EPOCHS = 40
+LEARNING_RATE = 1e-4
+NUM_CLASSES = 10
+PATCH_SIZE = 4
+IMG_SIZE = 28
+IN_CHANNELS = 1
+NUM_HEADS = 8
+DROPOUT = 0.001
+HIDDEN_DIM = 768
+ADAM_WEIGHT_DECAY = 0
+ADAM_BETAS = (0.9, 0.999)
+ACTIVATION = "gelu"
+NUM_ENCODERS = 4
+EMBED_DIM = (PATCH_SIZE ** 2) * IN_CHANNELS  # 16
+NUM_PATCHES = (IMG_SIZE // PATCH_SIZE) ** 2  # 49
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
