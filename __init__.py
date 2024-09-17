245e17a8be5de468567e29faf6a671014843ff0e
Yalanskyi Roman <keito.eternal@gmail.com>
2024-09-17 19:38:44 +0300

    initializations before implementing PaliGemma

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 __all__ = ['utils', 'tests', 'LogisticRegression', 'LinearRegression',
            'GLA', 'SVM', 'Tree', 'CNN', 'ObjectDetection',
-           'Transformers']
+           'Transformers', 'advanced']
