4773e2c2a76a861b1e7046475323bc671067e3e8
Yalanskyi Roman <keito.eternal@gmail.com>
2024-10-30 11:10:42 +0200

    added kMeans++ initialization

--- advanced/clustering/kMeans/kMeans.py
+++ advanced/clustering/kMeans/kMeans.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
+
 class kMeans:
     def __init__(self, k=3, max_iterations=500, epsilon=1e-5):
         self.k = k
@@ -12,12 +13,37 @@ class kMeans:
     def euclidean_distance(X, centroids):
         return np.sqrt(np.sum((centroids - X)**2, axis=1))
 
-    def fit(self, input):
-        self.centroids = np.random.uniform(
+    @staticmethod
+    def init_centroids(self, input):
+        centroids = np.random.uniform(
             np.amin(input, axis=0),
             np.amax(input, axis=0),
-            size=(self.k, input.shape[1]))
+            size=(1, input.shape[1]))
+
+        for idx in range(1, self.k):
+            max_distance = 0
+            next_centroid = None
+
+            for data_point in input:
+                centroids_distances = self.euclidean_distance(
+                    data_point, np.array(centroids))
+                if np.max(centroids_distances) > max_distance:
+                    max_distance = np.max(centroids_distances)
+                    next_centroid = data_point
+            
+            centroids = np.vstack((centroids, np.array(next_centroid)))
         
+        return np.array(centroids)
+
+    def fit(self, input, mode='default'):
+        if mode == '++':
+            self.centroids = self.init_centroids(self, input)
+        elif mode == 'default':
+            self.centroids = np.random.uniform(
+                np.amin(input, axis=0),
+                np.amax(input, axis=0),
+                size=(self.k, input.shape[1]))
+            
         for _ in range(self.max_iterations):
             centroids_elements_indices = [[] for i in range(self.k)]
             
@@ -47,14 +73,17 @@ class kMeans:
 
 random_input = np.random.randint(0, 100, (50, 2))
 k_means = kMeans()
-clustered_indices = k_means.fit(random_input)
+clustered_indices = k_means.fit(random_input, mode="++")
 colors = list(mcolors.TABLEAU_COLORS.keys())[:k_means.k]
 
 for cluster_idx, indices in enumerate(clustered_indices):
     cluster_points = random_input[indices]
-    plt.scatter(cluster_points[:, 0], cluster_points[:, 1], color=colors[cluster_idx], label=f'Cluster {cluster_idx+1}')
+    plt.scatter(cluster_points[:, 0], cluster_points[:, 1],
+                color=colors[cluster_idx],
+                label=f'Cluster {cluster_idx+1}')
 
-plt.scatter(k_means.centroids[:, 0], k_means.centroids[:, 1], s=100, color='black', marker="*", label='Centroids')
+plt.scatter(k_means.centroids[:, 0], k_means.centroids[:, 1],
+            s=100, color='black', marker="*", label='Centroids')
 
 plt.legend()
 plt.show()
