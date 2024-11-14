2c02e177666773ba0413ad22eb561fa41d7983e0
Yalanskyi Roman <keito.eternal@gmail.com>
2024-11-14 09:43:26 +0200

    finished MobileNetV3 architecture implementation

--- CNN/MobileNets/MobileNetV3.py
+++ CNN/MobileNets/MobileNetV3.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from torchsummary import summary 
 
 
 def _make_divisible(v, divisor, min_value=None):
@@ -47,39 +48,39 @@ class SELayer(nn.Module):
 
 
 class BottleneckConvLayer(nn.Module):
-    def __init__(self, in_channels, out_channels,
-                 stride=2, kernel_size=3, padding=0, t=6,
+    def __init__(self, in_channels, out_channels, hidden_dim,
+                 stride=2, kernel_size=3, padding=0,
                  residual=False, se=True, non_lin='RE'):
         super(BottleneckConvLayer, self).__init__()
         self.residual = residual
-        self.expansion_chls_n = round(in_channels * t)
+        self.hidden_dim = hidden_dim
         
         self.upsample_conv = nn.Sequential(
             nn.Conv2d(in_channels=in_channels,
-                      out_channels=self.expansion_chls_n,
+                      out_channels=self.hidden_dim,
                       kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(num_features=self.expansion_chls_n),
+            nn.BatchNorm2d(num_features=self.hidden_dim),
             nn.ReLU6() if non_lin == 'RE' else h_sigmoid()
         )
 
         self.depthwise = nn.Sequential(
-            nn.Conv2d(in_channels=self.expansion_chls_n,
-                      out_channels=self.expansion_chls_n,
+            nn.Conv2d(in_channels=self.hidden_dim,
+                      out_channels=self.hidden_dim,
                       kernel_size=kernel_size, stride=stride,
-                      padding=padding, groups=self.expansion_chls_n,
+                      padding=padding, groups=self.hidden_dim,
                       bias=False),
-            nn.BatchNorm2d(num_features=self.expansion_chls_n),
+            nn.BatchNorm2d(num_features=self.hidden_dim),
             nn.ReLU6() if non_lin == 'RE' else h_sigmoid()
         )
 
         self.downsample_conv = nn.Sequential(
-            nn.Conv2d(in_channels=self.expansion_chls_n,
+            nn.Conv2d(in_channels=self.hidden_dim,
                       out_channels=out_channels,
                       kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(num_features=out_channels),
         )
         if se:
-            self.se_layer = SELayer(self.expansion_chls_n)
+            self.se_layer = SELayer(self.hidden_dim)
 
         self.se = se
 
@@ -95,3 +96,114 @@ class BottleneckConvLayer(nn.Module):
         else:
             return x
 
+
+class DefaultConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 stride=2, kernel_size=3,
+                 padding=0, bn=True, non_lin=None):
+        super(DefaultConvLayer, self).__init__()
+
+        layers = [
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                stride=stride,
+                kernel_size=kernel_size,
+                padding=padding,
+                bias=not bn
+            )
+        ]
+        
+        if bn:
+            layers.append(nn.BatchNorm2d(num_features=out_channels))
+        
+        if non_lin == 'HS':
+            layers.append(h_swish())
+
+        self.layer = nn.Sequential(*[layer for layer in layers
+                                     if layer is not None])
+
+    def forward(self, x):
+        return self.layer(x)
+
+
+class MobileNetV3(nn.Module):
+    def __init__(self, n_classes, net_config, alpha=1):
+        super(MobileNetV3, self).__init__()
+        self.alpha = alpha
+
+        self.conv1 = DefaultConvLayer(
+            in_channels=3, out_channels=16, stride=2,
+            padding=1, non_lin='HS')
+
+        self.bottleneck_convs = nn.Sequential(
+            *self.create_bottleneck_conv_list(net_config)
+        )
+        
+        self.conv2 = DefaultConvLayer(
+            in_channels=160, out_channels=960, kernel_size=1,
+            stride=1, non_lin='HS')
+        
+        self.pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.conv3_nbn = DefaultConvLayer(
+            in_channels=960, out_channels=1280,
+            stride=1, kernel_size=1, bn=False, non_lin='HS'
+        )
+        self.conv4_nbn = DefaultConvLayer(
+            in_channels=1280, out_channels=n_classes,
+            stride=1, kernel_size=1, bn=False
+        )
+
+    def create_bottleneck_conv_list(self, backbone):
+        bottleneck_conv_list = []
+        for layer in backbone:
+            in_channels = round(layer[0] * self.alpha)
+            out_channels = round(layer[1] * self.alpha)
+            hidden_dim = round(layer[2] * self.alpha)
+            is_residual = True if layer[4] == 1 and in_channels == out_channels else False
+            bottleneck_conv_list.append(
+                BottleneckConvLayer(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=layer[3],
+                    hidden_dim=hidden_dim,
+                    stride=layer[4],
+                    padding=layer[5],
+                    residual=is_residual, se=layer[6],
+                    non_lin=layer[-1])
+            )
+        return bottleneck_conv_list
+            
+    def forward(self, input):
+        x = self.conv1(input)
+        x = self.bottleneck_convs(x)
+        x = self.conv2(x)
+        x = self.pool(x)
+        x = self.conv3_nbn(x)
+        x = self.conv4_nbn(x)
+        return x
+
+
+v3_large_config = [
+    #in_channels, out_channels, exp_channels, kernel_size, stride, padding, se, nl
+    [16, 16, 16, 3, 1, 1, False, 'RE'],
+    [16, 24, 64, 3, 2, 1, False, 'RE'],
+    [24, 24, 72, 3, 1, 1, False, 'RE'],
+    [24, 40, 72, 5, 2, 2, True, 'RE'],
+    [40, 40, 120, 5, 1, 2, True, 'RE'],
+    [40, 40, 120, 5, 1, 2, True, 'RE'],
+    [40, 80, 240, 3, 2, 1, False, 'HS'],
+    [80, 80, 200, 3, 1, 1, False, 'HS'],
+    [80, 80, 184, 3, 1, 1, False, 'HS'],
+    [80, 80, 184, 3, 1, 1, False, 'HS'],
+    [80, 112, 480, 3, 1, 1, True, 'HS'],
+    [112, 112, 672, 3, 1, 1, True, 'HS'],
+    [112, 160, 672, 5, 2, 2, True, 'HS'],
+    [160, 160, 960, 5, 1, 2, True, 'HS'],
+    [160, 160, 960, 5, 1, 2, True, 'HS'],
+
+]
+
+if __name__ == '__main__':
+    model = MobileNetV3(10, net_config=v3_large_config)
+    summary(model=model, input_size=(3, 224, 224))
