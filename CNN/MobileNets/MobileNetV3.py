b102e5847068ec72f57a9bc6f87cfa3eefb60c3b
Yalanskyi Roman <keito.eternal@gmail.com>
2024-11-14 16:19:20 +0200

    add small/large configs support - Fixed divisibility potential problems - Added classifier

--- CNN/MobileNets/MobileNetV3.py
+++ CNN/MobileNets/MobileNetV3.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from torchsummary import summary 
 
@@ -128,38 +129,64 @@ class DefaultConvLayer(nn.Module):
 
 
 class MobileNetV3(nn.Module):
-    def __init__(self, n_classes, net_config, alpha=1):
+    def __init__(self, n_classes, net_config, alpha=1, is_small=False):
         super(MobileNetV3, self).__init__()
         self.alpha = alpha
-
+        self.n_classes = n_classes
+        self.is_small = is_small
         self.conv1 = DefaultConvLayer(
-            in_channels=3, out_channels=16, stride=2,
+            in_channels=3, out_channels=_make_divisible(16 * self.alpha, 8), stride=2,
             padding=1, non_lin='HS')
 
         self.bottleneck_convs = nn.Sequential(
             *self.create_bottleneck_conv_list(net_config)
         )
         
-        self.conv2 = DefaultConvLayer(
-            in_channels=160, out_channels=960, kernel_size=1,
-            stride=1, non_lin='HS')
+        self.conv2 = self.create_second_def_conv(is_small)
         
         self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.conv3_nbn = DefaultConvLayer(
-            in_channels=960, out_channels=1280,
-            stride=1, kernel_size=1, bn=False, non_lin='HS'
+
+        self.classifier = self.create_classifier(is_small)
+
+    def create_classifier(self, is_small):
+        output_options = {'large': 1280, 'small': 1024}
+        input_options = {'large': 960, 'small': 576}
+        mode = "small" if is_small else "large"
+        
+        out_channels = _make_divisible(
+            output_options[mode] * self.alpha, 8)
+        in_channels = _make_divisible(
+            input_options[mode] * self.alpha, 8)
+        
+        return nn.Sequential(
+            nn.Linear(in_channels, out_channels),
+            h_swish(),
+            nn.Dropout(0.2),
+            nn.Linear(out_channels, self.n_classes),
         )
-        self.conv4_nbn = DefaultConvLayer(
-            in_channels=1280, out_channels=n_classes,
-            stride=1, kernel_size=1, bn=False
-        )
+
+    def create_second_def_conv(self, is_small):
+        if not is_small:
+            in_channels = _make_divisible(160 * self.alpha, 8)
+            out_channels = _make_divisible(960 * self.alpha, 8)
+            return DefaultConvLayer(
+                in_channels=in_channels,
+                out_channels=out_channels, kernel_size=1,
+                stride=1, non_lin='HS')
+        else:
+            in_channels = _make_divisible(96 * self.alpha, 8)
+            out_channels = _make_divisible(576 * self.alpha, 8)
+            return DefaultConvLayer(
+                    in_channels=in_channels,
+                    out_channels=out_channels, kernel_size=1,
+                    stride=1, non_lin='HS')
 
     def create_bottleneck_conv_list(self, backbone):
         bottleneck_conv_list = []
         for layer in backbone:
-            in_channels = round(layer[0] * self.alpha)
-            out_channels = round(layer[1] * self.alpha)
-            hidden_dim = round(layer[2] * self.alpha)
+            in_channels = _make_divisible(layer[0] * self.alpha, 8)
+            out_channels = _make_divisible(layer[1] * self.alpha, 8)
+            hidden_dim = _make_divisible(layer[2] * self.alpha, 8)
             is_residual = True if layer[4] == 1 and in_channels == out_channels else False
             bottleneck_conv_list.append(
                 BottleneckConvLayer(
@@ -179,13 +206,13 @@ class MobileNetV3(nn.Module):
         x = self.bottleneck_convs(x)
         x = self.conv2(x)
         x = self.pool(x)
-        x = self.conv3_nbn(x)
-        x = self.conv4_nbn(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
         return x
 
 
 v3_large_config = [
-    #in_channels, out_channels, exp_channels, kernel_size, stride, padding, se, nl
+    # in_channels, out_channels, exp_channels, kernel_size, stride, padding, se, nl
     [16, 16, 16, 3, 1, 1, False, 'RE'],
     [16, 24, 64, 3, 2, 1, False, 'RE'],
     [24, 24, 72, 3, 1, 1, False, 'RE'],
@@ -204,6 +231,25 @@ v3_large_config = [
 
 ]
 
+v3_small_config = [
+    # in_channels, out_channels, exp_channels, kernel_size, stride, padding, se, nl
+    [16, 16, 16, 3, 2, 1, True, 'RE'],
+    [16, 24, 72, 3, 2, 1, False, 'RE'],
+    [24, 24, 88, 3, 1, 1, False, 'RE'],
+    [24, 40, 96, 5, 2, 2, True, 'HS'],
+    [40, 40, 240, 5, 1, 2, True, 'HS'],
+    [40, 40, 240, 5, 1, 2, True, 'HS'],
+    [40, 48, 120, 5, 1, 2, True, 'HS'],
+    [48, 48, 144, 5, 1, 2, True, 'HS'],
+    [48, 96, 288, 5, 2, 2, True, 'HS'],
+    [96, 96, 576, 5, 1, 2, True, 'HS'],
+    [96, 96, 576, 5, 1, 2, True, 'HS']
+]
+
+
 if __name__ == '__main__':
-    model = MobileNetV3(10, net_config=v3_large_config)
-    summary(model=model, input_size=(3, 224, 224))
+    model1 = MobileNetV3(10, net_config=v3_small_config, is_small=True)
+    summary(model=model1, input_size=(3, 224, 224))
+
+    model2 = MobileNetV3(10, net_config=v3_large_config, is_small=False)
+    summary(model=model2, input_size=(3, 224, 224))
